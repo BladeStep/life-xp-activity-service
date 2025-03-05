b9cdd8abedb7d1d4e91180538b419c0d3db34c0f
Msp88 <aleast.88@gmail.com>
2025-03-05 21:43:41 +0300

    Initial commit for new repository

---
+++ life-xp-activity-service-main/src/test/java/com/bladestepapp/lifexpxpservicemain/LifeXpXpServiceMainApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bladestepapp.lifexpxpservicemain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeXpXpServiceMainApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("Context was upped");
+    }
+
+}
